c5ae49069fe0788a3f073fb9c67dfabce7d15359
Ganto <i@ganto.icu>
2023-08-04 11:52:43 +0800

    github first commit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="footer">Copyright © 2009 - 2023 快速批量打开网站 All Rights Reserved. <a href="http://www.miitbeian.gov.cn/" target="_blank">域ICP备19972023号</a></div>
+    <div class="footer">
+        <p>Copyright © 2009 - 2023 快速批量打开网站 All Rights Reserved. <a href="http://www.miitbeian.gov.cn/" target="_blank">域ICP备19972023号</a></p>
+        <p>欢迎提出建议：<a href="https://gitee.com/fast" target="_blank" >Gitee</a></p>
+    </div>
 </template>
 
 <script setup>
@@ -14,8 +17,14 @@
     padding: 10px 5px;
     border-radius: 10px;
 
-    a{
-        color: #6c7bff;
+    p{
+        a{
+            color: #6c7bff;
+        }
+    }
+
+    p + p{
+        margin-top: 5px;
     }
 }
 </style>
